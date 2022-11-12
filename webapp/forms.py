a9b394e7f521074d7e18bdd20acf23b2abb5c884
Marat <marat.mashanlo@gmail.com>
2022-11-12 17:57:09 +0600

    save pered bonusom 2

--- webapp/forms.py
+++ webapp/forms.py
@@ -3,16 +3,16 @@ from django.forms import widgets
 
 
 class RecordForm(forms.Form):
-    title = forms.CharField(max_length=100, required=True, label='Название', error_messages={
-        'required': "Поле обязательное для заполнение"},
+    title = forms.CharField(max_length=100, required=True, label='Nazvanie', error_messages={
+        'required': "Zapolnit obyazatelno"},
                             widget=widgets.TextInput(attrs={'rows': 3, 'class': 'form-control mb-3'}))
-    email_author = forms.EmailField(max_length=40, required=True, label='Е-маил', error_messages={
-        'required': "Поле обязательное для заполнение"},
+    email_author = forms.EmailField(max_length=40, required=True, label='Е-mail', error_messages={
+        'required': "Zapolnit obyazatelno"},
                                     widget=widgets.TextInput(attrs={'rows': 3, 'class': 'form-control mb-3'}))
-    content = forms.CharField(max_length=2000, required=True, label='Подробная описание',
+    content = forms.CharField(max_length=2000, required=True, label='Opisanie',
                               widget=widgets.Textarea(attrs={'rows': 6, 'class': 'form-control mb-3'}))
 
 
 class SearchForm(forms.Form):
-    title = forms.CharField(max_length=100, required=True, label='Название', error_messages={
-        'required': "Поле обязательное для заполнение"})
+    title = forms.CharField(max_length=100, required=True, label='Nazvanie', error_messages={
+        'required': "Zapolnit obyazatelno"})
--- webapp/models.py
+++ webapp/models.py
@@ -3,12 +3,13 @@ STATUS_CHOICES = [('active', 'Активно'), ('blocked', 'Заблокиро�
 
 
 class GuestBook(models.Model):
-    title = models.CharField(max_length=100, null=False, blank=False, verbose_name='Название')
-    status = models.CharField(max_length=40, choices=STATUS_CHOICES, default='active', verbose_name='Статус')
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name='Nazvanie')
+    status = models.CharField(max_length=40, choices=STATUS_CHOICES, default='active', verbose_name='Status')
     email_author = models.EmailField(max_length=30, verbose_name='E-mail')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
-    content = models.TextField(max_length=2000, verbose_name='Подробное описание')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Data Sozdaniya")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Data Izmeneniya")
+    content = models.TextField(max_length=2000, verbose_name='Opisanie')
+
 
     def __str__(self):
         return f"{self.pk}, {self.title}, {self.status}, {self.email_author}, {self.content}, {self.created_at}, {self.updated_at}"
