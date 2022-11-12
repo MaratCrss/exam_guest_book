73ba7742f78ebd5ee3f0a846b5eec0b76e54b3c0
Marat <marat.mashanlo@gmail.com>
2022-11-12 17:33:25 +0600

    sdelal 4 i 5 etap

--- exam_guest_book/urls.py
+++ exam_guest_book/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.urls import path
 
-from webapp.views import index_view, search_record, create_record
+from webapp.views import index_view, create_record, delete_record, update_record, search_record
 
 urlpatterns = [
     path('', index_view, name='index'),
     path('create_record/', create_record, name='create_record'),
+    path('delete_record/<int:pk>/', delete_record, name='delete_record'),
+    path('update_record/<int:pk>/', update_record, name='update_record'),
     path('search_record/', search_record, name='search_record'),
-
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
-
-from webapp.models import GuestBook
 from webapp.forms import RecordForm
+from webapp.models import GuestBook
 
 
 def index_view(request):
@@ -11,13 +10,6 @@ def index_view(request):
     context = {"records": records}
     return render(request, "index.html", context)
 
-
-
-def search_record(request):
-    if request.method == "GET":
-        name = request.GET.get('name')
-        record = GuestBook.objects.filter(title__icontains=name)
-        return render(request, 'search_by_name.html', {'records': record})
 
 def create_record(request):
     if request.method == "GET":
@@ -31,3 +23,41 @@ def create_record(request):
             GuestBook.objects.create(title=title, email_author=email_author, content=content)
             return redirect("index")
     return render(request, "create_record.html", {'form': form})
+
+
+def update_record(request, **kwargs):
+    pk = kwargs['pk']
+    record = get_object_or_404(GuestBook, pk=pk)
+    if request.method == "GET":
+        form = RecordForm(initial={
+            'title': record.title,
+            'email_author': record.email_author,
+            'content': record.content
+        })
+        return render(request, "update_record.html", {'form': form})
+    else:
+        form = RecordForm(data=request.POST)
+        if form.is_valid():
+            record.title = form.cleaned_data.get("title")
+            record.email_author = form.cleaned_data.get("email_author")
+            record.content = form.cleaned_data.get("content")
+            record.save()
+            return redirect("index")
+        return render(request, "update_record.html", {'form': form})
+
+
+def delete_record(request, **kwargs):
+    pk = kwargs['pk']
+    record = get_object_or_404(GuestBook, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'delete_record.html', {'record': record})
+    else:
+        record.delete()
+        return redirect('index')
+
+
+def search_record(request):
+    if request.method == "GET":
+        name = request.GET.get('name')
+        record = GuestBook.objects.filter(title__icontains=name)
+        return render(request, 'search_by_name.html', {'records': record})
