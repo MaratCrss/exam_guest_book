54102a19fa290d82a58460f67cbb8803d7c9ffe3
Marat <marat.mashanlo@gmail.com>
2022-11-12 17:00:33 +0600

    2 etap

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
+
+from webapp.models import GuestBook
+
+
+def index_view(request):
+    records = GuestBook.objects.order_by('-created_at').filter(status='active')
+    context = {"records": records}
+    return render(request, "index.html", context)
+
+
+
+def search_record(request):
+    if request.method == "GET":
+        name = request.GET.get('name')
+        record = GuestBook.objects.filter(title__icontains=name)
+        return render(request, 'search_by_name.html', {'records': record})
